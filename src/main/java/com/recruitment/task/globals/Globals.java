5999ac8cdea3e10d8d60579da9059f53e72b1678
eryk-slowinski <e.slowinski92@gmail.com>
2022-01-19 09:32:36 +0100

    Created class with global variables

---
+++ src/main/java/com/recruitment/task/globals/Globals.java
@@ -0,0 +1,6 @@
+package com.recruitment.task.globals;
+
+public class Globals {
+    public static final String TRANSACTIONS_KEY = "transaction";
+    public static final String FEE_WAGES_KEY = "fee_wages";
+}
