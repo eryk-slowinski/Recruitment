97ba761cd07cc1d16cb8007d69bb6d6e3675b36b
eryk-slowinski <e.slowinski92@gmail.com>
2022-01-19 20:49:53 +0100

    Added loadUsers() method to main class

--- src/main/java/com/recruitment/task/RecruitmentApplication.java
+++ src/main/java/com/recruitment/task/RecruitmentApplication.java
@@ -1,6 +1,7 @@
 package com.recruitment.task;
 
 
+import com.recruitment.task.globals.Globals;
 import com.recruitment.task.utils.CSVHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ public class RecruitmentApplication {
 
 	public static void main(String[] args) {
 		CSVHandler.loadFromDir("src/main/java/com/recruitment/task/data");
+		Globals.loadUsers();
 		SpringApplication.run(RecruitmentApplication.class, args);
 	}
 }
